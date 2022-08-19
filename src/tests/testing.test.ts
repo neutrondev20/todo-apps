da2abce1579640601d6562a59eb58e0cd896c0b1
neutrondev20 <renaldi@lucys.app>
2022-08-19 11:25:30 +0800

    Add testing

--- src/tests/testing.test.ts
+++ src/tests/testing.test.ts
@@ -1,8 +1,21 @@
 import { addTask, updateTask } from './../components/todo/Todo';
-import { it, describe, expect } from "vitest"
+import { it, test, expect } from "vitest"
 
-describe("just testing test", () => {
-    it("can test", () => {
-        expect(true).toBe(true)
-    })
+test("add task & update task testing", () => {
+    type Mission = {
+        id : number,
+        text : string,
+        condition : boolean,
+        show : boolean
+    }
+
+    const missions : Mission[] = []
+
+    expect(addTask("hello", missions)).toBeUndefined()
+
+    expect(missions).toHaveLength(1)
+
+    expect(updateTask(missions[0], "world")).toBeUndefined();
+
+    expect(missions[0].text).toEqual("world")
 })
